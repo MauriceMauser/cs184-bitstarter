6f0bab3d17f36fc67eac79033c5341a46ab3ac4c
Ubuntu <ubuntu@ip-172-31-12-120.us-west-1.compute.internal>
2013-08-07 22:38:42 +0000

    changed web.js to read file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var html_file = fs.readFileSync(index.html); 
+var buffer = new Buffer(html_file, "utf-8");
+var hello_text = buffer.toString("utf-8")
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(hello_text);
 });
 
 var port = process.env.PORT || 8080;
